d31e6e7553c540ddfcf5f2ce1aed524a279de716
David Diallo <woof@dogeswap.org>
2022-07-13 21:23:16 +0200

    build: migrate to hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.16",
+};
+
+export default config;
