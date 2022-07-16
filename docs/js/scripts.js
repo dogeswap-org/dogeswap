28d53b1617dadb5c1bbf27b5ae85042432163be3
David Diallo <woof@dogeswap.org>
2022-07-16 18:32:44 +0200

    style: prettify docs

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -7,24 +7,24 @@
 // Scripts
 //
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav')
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: '#mainNav',
-      offset: 74,
-    })
-  }
+window.addEventListener("DOMContentLoaded", event => {
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: "#mainNav",
+            offset: 74,
+        });
+    }
 
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler')
-  const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'))
-  responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-      if (window.getComputedStyle(navbarToggler).display !== 'none') {
-        navbarToggler.click()
-      }
-    })
-  })
-})
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(document.querySelectorAll("#navbarResponsive .nav-link"));
+    responsiveNavItems.map(function(responsiveNavItem) {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
+                navbarToggler.click();
+            }
+        });
+    });
+});
