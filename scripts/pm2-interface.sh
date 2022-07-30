8a90cf0ed763e0c37a35672bb660e46d4267adf1
David Diallo <woof@dogeswap.org>
2022-07-30 15:02:33 +0200

    build: update pm2 script

--- scripts/pm2-interface.sh
+++ scripts/pm2-interface.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "${0%/*}/.."
-pm2 start packages/interface/dist/server/main.js --name @dogeswap/interface --watch
+pm2 start packages/interface/dist/server/server.js --name @dogeswap/interface --watch
